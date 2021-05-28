7e7e84de29c2d9c4f787b2cad9e31b9b902bbc8e
Lucas Loudon <lucasloudon@gmail.com>
2021-05-28 20:25:15 +0100

    Version 1.0.1

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
           <img
             className="landing-image"
             src={process.env.PUBLIC_URL + "/images/sapiens.png"}
-            alt="landing"
+            alt=""
           />
         </div>
       </div>
